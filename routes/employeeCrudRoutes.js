d70a7ba840e98ab9590b2d3f62645a523522541f
vedangipatel-prydan <vedupatel810@gmail.com>
2024-03-28 07:19:42 +0530

    Routes Modified

--- routes/employeeCrudRoutes.js
+++ routes/employeeCrudRoutes.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const FetchAllEmployees = require('../controllers/employees_controller');
-const CreateEmployee = require('../controllers/createEmployee');
-const getEmployeeById = require('../controllers/employeeById');
-const UpdateEmployee = require('../controllers/updateEmployee');
-const DeleteEmployee = require('../controllers/deleteEmployee');
-const EmployeeLogIn = require('../controllers/employeeLogIn');
-const employeeRoutes = express.Router();
+// const express = require('express');
+// const FetchAllEmployees = require('../controllers/employees_controller');
+// const CreateEmployee = require('../controllers/createEmployee');
+// const getEmployeeById = require('../controllers/employeeById');
+// const UpdateEmployee = require('../controllers/updateEmployee');
+// const DeleteEmployee = require('../controllers/deleteEmployee');
+// const EmployeeLogIn = require('../controllers/employeeLogIn');
+// const employeeRoutes = express.Router();
 
-employeeRoutes.get('/', (req, res) => {
-    res.send("Home Page!")
-})
+// employeeRoutes.get('/', (req, res) => {
+//     res.send("Home Page!")
+// })
 
-employeeRoutes.get('/fetchAllEmpoyees', FetchAllEmployees);
+// // employeeRoutes.get('/fetchAllEmpoyees', FetchAllEmployees);
 
-employeeRoutes.get('/createEmployee', CreateEmployee);
+// employeeRoutes.get('/createEmployee', CreateEmployee);
 
-employeeRoutes.get('/getEmployeeById', getEmployeeById);
+// employeeRoutes.get('/getEmployeeById', getEmployeeById);
 
-employeeRoutes.get('/UpdateEmployee', UpdateEmployee);
+// employeeRoutes.get('/UpdateEmployee', UpdateEmployee);
 
-employeeRoutes.get('/DeleteEmployee', DeleteEmployee);
+// employeeRoutes.get('/DeleteEmployee', DeleteEmployee);
 
-employeeRoutes.get('/LogIn', EmployeeLogIn);
+// employeeRoutes.get('/LogIn', EmployeeLogIn);
 
-// app.listen(3000);
+// // app.listen(3000);
 
-module.exports = employeeRoutes;
+// module.exports = employeeRoutes;
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
+const FetchAllEmployees = require('./controllers/employees_controller');
+const CreateEmployee = require('./controllers/createEmployee');
+const getEmployeeById = require('./controllers/employeeById');
+const UpdateEmployee = require('./controllers/updateEmployee');
+const DeleteEmployee = require('./controllers/deleteEmployee');
+const EmployeeLogIn = require('./controllers/employeeLogIn');
 const sequelize = require("./config/dbConfig");
-const EmployeeCrudRoutes = require("./routes/employeeCrudRoutes");
+
+const bodyParser = require("body-parser");
+const EmployeeCrudRoutes = express.Router();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+EmployeeCrudRoutes.get('/fetchAllEmpoyees', FetchAllEmployees);
+
+EmployeeCrudRoutes.get('/createEmployee', CreateEmployee);
+
+EmployeeCrudRoutes.get('/getEmployeeById', getEmployeeById);
+
+EmployeeCrudRoutes.get('/UpdateEmployee', UpdateEmployee);
+
+EmployeeCrudRoutes.get('/DeleteEmployee', DeleteEmployee);
+
+EmployeeCrudRoutes.get('/LogIn', EmployeeLogIn);
+
 app.use(EmployeeCrudRoutes);
 
 sequelize;
