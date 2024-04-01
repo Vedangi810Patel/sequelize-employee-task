477cab9411b7ff9379ce99300c187ab5e8244fb1
vedangipatel-prydan <vedupatel810@gmail.com>
2024-04-01 22:21:49 +0530

    Insert Book Through React

--- front-end/react-routing/src/App.js
+++ front-end/react-routing/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/HomePage.js';
 import FetchAllBookData from './components/FetchAllBookData.js' 
+import BookCrudReact from './components/BookCrudReact.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/fetchAllBooks' element={<FetchAllBookData />} />
+            <Route path='/insertBooks' element={<BookCrudReact />} />
           </Routes>
         </BrowserRouter>
       </header>
--- front-end/react-routing/src/components/BookCrudReact.js
+++ front-end/react-routing/src/components/BookCrudReact.js
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import xhttprequest from 'xhttprequst';
 
 const BookCrudReact = () => {
 
-    const xhttprequst = xhttprequst();
+    const [formData, setFormData] = useState({
+        book_id : '',
+        title: '',
+        book_description: '',
+        publish_year: 1234,
+        quantity_available: 0
+    });
+
+    const handleChange = e => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/insertBooks', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    alert('Book added successfully');
+                    setFormData({
+                        book_id: 1,
+                        title: '',
+                        book_description: '',
+                        publish_year: 1234,
+                        quantity_available: 0
+                    });
+                } else {
+                    console.error('Error adding book:', xhr.statusText);
+                    alert('Error adding book');
+                }
+            }
+        };
+        xhr.send(JSON.stringify(formData));
+    };
 
     return (
         <div className='main-div'>
             {/* <h1> Form Creation </h1> */}
-            <form onSubmit={PrintValues}>
+            <form onSubmit={handleSubmit}>
                 <label id='book_id' htmlFor="book_id"> Book Id : </label>
-                <input type="number" name="book_id" id="book_id" placeholder="1" /><br />
+                <input type="number" name="book_id" id="book_id" value={formData.book_id} placeholder="1" onChange={handleChange}/><br />
 
                 <label id='title' htmlFor="title"> Book Name : </label>
-                <input type="text" name="title" id="title" placeholder="Cloud Nine" /> <br />
+                <input type="text" name="title" id="title" value={formData.title} placeholder="Cloud Nine" onChange={handleChange}/> <br />
 
-                <label id='book_description' htmlFor="book_description"> Mob No : </label>
-                <input type="text" name="book_description" id="book_description" placeholder="Write Something About Book" /> <br />
+                <label id='book_description' htmlFor="book_description"> BookDescription : </label>
+                <input type="text" name="book_description" id="book_description" value={formData.book_description} placeholder="Write Something About Book" onChange={handleChange}/> <br />
 
-                <label id='' htmlFor="address"> Address : </label>
-                <textarea name="address" rows="5" cols="20" placeholder="type your address here." /> <br />
+                <label id='publish_year' htmlFor="publish_year"> Pulish Year : </label>
+                <input type="number" id="publish_year" name="publish_year" value={formData.publish_year} placeholder="1997" onChange={handleChange} /> <br />
+
+                <label id='quantity_available' htmlFor="quantity_available"> Quantity Available : </label>
+                <input type="number" id="quantity_available" name="quantity_available" value={formData.quantity_available} placeholder="1997" onChange={handleChange}/> <br />
+
+                <button type="submit">Add Book</button>
             </form>
         </div>
     )
